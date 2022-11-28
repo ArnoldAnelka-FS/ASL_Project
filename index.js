720b55096bdeb3b308f2535d088b3462d5467cc5
Anelka Arnold <anelkaarnold@Anelkas-MBP.lan>
2022-11-27 23:49:12 -0500

    assignment restful

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+const productRouter = require('./routes/Products');
 
-// GET / HTTP/1.1
-app.get('/', (request, response) => {
-    response.send('Home Page!')
-});
-
-// GET /products/8719-small-red HTTP/1.1
-
-app.get('/products/:productId-:productName', (request, response) => {
-    response.send('GET Products:' + request.params.productId + request.params.productName)
-})
-
-// GET /products/8719 HTTP/1.1
-
-app.get('/products/:productId', (request, response) => {
-    response.send('GET Products:' + request.params.productId)
-})
-
-// GET /products/all HTTP/1.1
-app.get('/products/:all', (request, response) => {
-    response.send('GET Products: ' + request.params.all)
-})
+app.use("/products", productRouter)
 
 app.listen(3000)
